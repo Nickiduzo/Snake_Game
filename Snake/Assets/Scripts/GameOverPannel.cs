8e4f66deb25eafbb9259cccf63fe49e36c0b44db
NOA <daroter38@gmail.com>
2023-09-01 19:27:32 +0300

    Code

---
+++ Snake/Assets/Scripts/GameOverPannel.cs
@@ -0,0 +1,12 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class GameOverPannel : MonoBehaviour
+{
+    public float fadeInDuration = 1f; // ѕродолжительность анимации по€влени€
+
+    void Start()
+    {
+
+    }
+}
--- Snake/Assets/Scripts/Snake.cs
+++ Snake/Assets/Scripts/Snake.cs
@@ -1,47 +1,103 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Snake : MonoBehaviour
 {
-    public float moveSpeed = 1.0f;
-    public float gridSize = 1.0f;
-
-    private Vector3 moveDirection = Vector3.right; // Начальное направление - вправо
-    private Vector3 nextMovePosition; // Следующая позиция для движения
-    private float nextMoveTime = 0; // Время следующего шага
+    [SerializeField] private GameObject losePanel;
+    private Vector2 currentDirection = Vector2.right; // Текущее направление движения
+    private float timeToMove = 0.5f;
+    private List<Transform> bodySegments = new List<Transform>(); // Сегменты тела змейки
 
     void Start()
     {
-        nextMovePosition = transform.position + moveDirection * gridSize;
-        nextMoveTime = Time.time + moveSpeed;
-    }
-
-    public void RotateRight()
-    {
-        if (moveDirection != Vector3.left) // Запрещаем разворачиваться назад
-        {
-            moveDirection = Vector3.right;
-            transform.rotation = Quaternion.Euler(0, 0, 0); // Поворот вправо
-        }
-    }
-
-    public void RotateLeft()
-    {
-        if (moveDirection != Vector3.right) // Запрещаем разворачиваться назад
-        {
-            moveDirection = Vector3.left;
-            transform.rotation = Quaternion.Euler(0, 180, 0); // Поворот влево
-        }
+        // Изначально змейка состоит из двух сегментов
+        bodySegments.Add(transform);
+        CreateBodySegment();
     }
 
     void FixedUpdate()
     {
-        Move();
+        if (timeToMove <= 0f)
+        {
+            MoveSnake();
+            timeToMove = 0.5f;
+        }
+        else
+        {
+            timeToMove -= Time.fixedDeltaTime;
+        }
     }
 
-    void Move()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        transform.position = nextMovePosition;
-        nextMovePosition += moveDirection * gridSize;
-        nextMoveTime = Time.time + moveSpeed;
+        if (collision.gameObject.CompareTag("Block"))
+        {
+            Time.timeScale = 0f;
+            losePanel.SetActive(true);
+        }
+    }
+
+    // Создать новый сегмент тела змейки
+    private void CreateBodySegment()
+    {
+        GameObject segment = new GameObject("SnakeSegment");
+        segment.transform.position = bodySegments[bodySegments.Count - 1].position;
+        bodySegments.Add(segment.transform);
+    }
+
+    // Повернуть влево
+    public void TurnLeft()
+    {
+        if (currentDirection == Vector2.up)
+        {
+            currentDirection = Vector2.left;
+        }
+        else if (currentDirection == Vector2.left)
+        {
+            currentDirection = Vector2.down;
+        }
+        else if (currentDirection == Vector2.down)
+        {
+            currentDirection = Vector2.right;
+        }
+        else if (currentDirection == Vector2.right)
+        {
+            currentDirection = Vector2.up;
+        }
+    }
+
+    // Повернуть вправо
+    public void TurnRight()
+    {
+        if (currentDirection == Vector2.up)
+        {
+            currentDirection = Vector2.right;
+        }
+        else if (currentDirection == Vector2.right)
+        {
+            currentDirection = Vector2.down;
+        }
+        else if (currentDirection == Vector2.down)
+        {
+            currentDirection = Vector2.left;
+        }
+        else if (currentDirection == Vector2.left)
+        {
+            currentDirection = Vector2.up;
+        }
+    }
+
+    // Двигать змейку
+    private void MoveSnake()
+    {
+        // Двигаем каждый сегмент тела
+        for (int i = bodySegments.Count - 1; i > 0; i--)
+        {
+            bodySegments[i].position = bodySegments[i - 1].position;
+        }
+
+        // Двигаем голову
+        Vector2 newHeadPosition = (Vector2)transform.position + currentDirection * 0.25f;
+        transform.position = newHeadPosition;
     }
 }
